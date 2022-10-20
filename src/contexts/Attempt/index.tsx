240c454b15e86b6d61377d01909f76390c86e514
gabrielmeloc22 <gabrielpresb@gmail.com>
2022-10-20 16:53:31 -0300

    refactor(attemptContext): add game lifecycle state and clearError function

--- src/contexts/Attempt/index.tsx
+++ src/contexts/Attempt/index.tsx
@@ -4,6 +4,8 @@ type State = {
   curr: number;
   attempts: Array<string[]>;
   error: boolean;
+  gameOver: boolean;
+  hasWon: boolean;
 };
 
 type Action = {
@@ -11,8 +13,8 @@ type Action = {
   payload: Partial<State>;
 };
 
-interface AttemptContextData {
-  attempt: State;
+interface AttemptContextData extends State {
+  clearError: () => void;
   sendAnswer: (answer: string[]) => void;
 }
 
@@ -25,10 +27,13 @@ const AttemptContext = createContext({} as AttemptContextData);
 const reducer = (state: State, { payload, type }: Action): State => {
   switch (type) {
     case "sendAnswer":
+      const hasWon = payload.attempts?.some((attempt) => attempt.join("") === "carro") || false;
       return {
         ...state,
         error: false,
         curr: state.curr + 1,
+        hasWon,
+        gameOver: state.curr === 6 || hasWon,
         attempts: [...state.attempts, ...payload?.attempts!],
       };
     case "setError":
@@ -45,6 +50,8 @@ const initialState: State = {
   attempts: [],
   curr: 1,
   error: false,
+  hasWon: false,
+  gameOver: false,
 };
 
 export function AttemptProvider({ children }: AttemptProvider) {
@@ -59,20 +66,23 @@ export function AttemptProvider({ children }: AttemptProvider) {
           error: true,
         },
       });
-      setTimeout(() => {
-        dispatch({
-          type: "setError",
-          payload: {
-            error: false,
-          },
-        });
-      }, 500);
       return;
     }
     dispatch({ payload: { attempts: [answer] }, type: "sendAnswer" });
   }
 
-  return <AttemptContext.Provider value={{ attempt: state, sendAnswer }}>{children}</AttemptContext.Provider>;
+  function clearError() {
+    dispatch({
+      type: "setError",
+      payload: {
+        error: false,
+      },
+    });
+  }
+
+  return (
+    <AttemptContext.Provider value={{ ...state, sendAnswer, clearError }}>{children}</AttemptContext.Provider>
+  );
 }
 
 export const useAttempt = () => {
