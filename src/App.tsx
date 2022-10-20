4f5d677cb27c2eac9d6ecc6fc02960eb4656b36d
gabrielmeloc22 <gabrielpresb@gmail.com>
2022-10-20 16:56:55 -0300

    feat(app): implement toast component and game lifecycle

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
+import { ToastProvider } from "@radix-ui/react-toast";
 import { AnsweredRow } from "./components/AnsweredRow";
 import { Flex, Heading } from "./components/Primitives";
+import { Toast, ToastDescription, ToastViewport } from "./components/Toast";
 import { WordRow } from "./components/WordRow";
 import { useAttempt } from "./contexts/Attempt";
 import { globalStyles } from "./styles/globalStyles";
 
 export function App() {
   globalStyles();
-  const {
-    attempt: { curr, error, attempts },
-  } = useAttempt();
+  const { clearError, curr, error, attempts, gameOver } = useAttempt();
 
   return (
     <Flex direction="column" align="center">
-      {error && <Heading onAnimationEnd={() => {}}>Erro</Heading>}
+      <ToastProvider>
+        <Toast
+          open={error}
+          onOpenChange={clearError}
+          css={{
+            backgroundColor: "$slate3",
+          }}
+        >
+          <ToastDescription>Apenas palavras de cinco letras!</ToastDescription>
+        </Toast>
+        <ToastViewport />
+      </ToastProvider>
       <Heading
         size="md"
         css={{
@@ -22,15 +33,17 @@ export function App() {
       >
         Wordle
       </Heading>
+
       <Flex
         direction="column"
         css={{
+          marginTop: "2rem",
           marginBottom: "5rem",
         }}
       >
         {new Array(6).fill(null).map((_, i) => {
           return attempts[i] === undefined ? (
-            <WordRow key={i} active={i + 1 === curr} />
+            <WordRow key={i} active={i + 1 === curr && !gameOver} />
           ) : (
             <AnsweredRow key={i} answer={attempts[i]} />
           );
